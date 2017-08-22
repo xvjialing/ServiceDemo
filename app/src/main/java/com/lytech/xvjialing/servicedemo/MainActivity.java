3ce65aa6d046fe9ddba6629ba5bc7ac7f313a222
xvjialing <xvjialing@outlook.com>
2017-08-22 15:41:49 +0800

    启动和关闭服务

--- app/src/main/java/com/lytech/xvjialing/servicedemo/MainActivity.java
+++ app/src/main/java/com/lytech/xvjialing/servicedemo/MainActivity.java
@@ -1,13 +1,41 @@
 package com.lytech.xvjialing.servicedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_startService)
+    Button btnStartService;
+    @BindView(R.id.btn_stopService)
+    Button btnStopService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick({R.id.btn_startService, R.id.btn_stopService})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_startService:
+                Intent intent=new Intent(this,MyService.class);
+                startService(intent);
+                break;
+            case R.id.btn_stopService:
+                Intent intent2=new Intent(this,MyService.class);
+                stopService(intent2);
+                break;
+        }
     }
 }
